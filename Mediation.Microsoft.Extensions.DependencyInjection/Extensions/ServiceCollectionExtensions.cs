7d14d30bbfe5eb90a3a51116d8349bf39a14c3e7
Jeremy Ingraham <jeremy@ingraham.us>
2019-11-09 23:56:25 -0500

    Started adding back in dependency injection

---
+++ Mediation.Microsoft.Extensions.DependencyInjection/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Mediation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection {
+    public static class ServiceCollectionExtensions {
+        struct TypeSearchConfiguration {
+            public bool AllowMultipleHandlers { get; }
+
+            public TypeSearchConfiguration(
+                bool allowMultipleHandlers) {
+                AllowMultipleHandlers = allowMultipleHandlers;
+            }
+        }
+
+        static IReadOnlyDictionary<Type, TypeSearchConfiguration> TypeSearches =
+            new Dictionary<Type, TypeSearchConfiguration> {
+                { typeof(IRequestHandler<,>), new TypeSearchConfiguration(false) },
+                { typeof(INotificationHandler<>), new TypeSearchConfiguration(true) }
+            };
+
+        #region AddMediator
+        public static IServiceCollection AddMediator(
+            this IServiceCollection services) {
+            return services
+                .AddTransient<MediationServiceProvider>(provider => provider.GetService);
+        }
+        #endregion
+
+        internal static IEnumerable<Type> ApplyMediationFilter(
+            this IEnumerable<Type> types) {
+            var concretions = types.Where(x => x.IsConcrete());
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mediation.Microsoft.Extensions.DependencyInjection/Extensions/TypeExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System {
+    public static class TypeExtensions {
+        public static bool IsConcrete(
+            this Type type)
+            => !type.IsInterface && !type.IsAbstract;
+
+        public static bool IsOpenGeneric(
+            this Type type)
+            => type.IsGenericType || type.ContainsGenericParameters;
+
+        public static IEnumerable<Type> GetBaseTypes(this Type type) {
+            for (var current = type; current != typeof(object); current = current.BaseType)
+                yield return current.BaseType;
+        }
+
+        public static IEnumerable<Type> MatchGenericTypeDefinitions(this Type type, Type template) {
+            return (template.IsInterface ? type.GetInterfaces() : type.GetBaseTypes())
+                .Where(x => x.IsGenericType && x.GetGenericTypeDefinition() == template);
+        }
+
+        public static bool MatchesInterfaceTypeArguments(this Type type, Type template) {
+            if (!template.IsInterface) throw new ArgumentException("Type not an interface", nameof(template));
+            if (!type.IsInterface) type = type.GetInterface(template.Name);
+            return type.GenericTypeArguments.SequenceEqual(template.GenericTypeArguments);
+        }
+
+        public static bool TryMakeGenericType(this Type type, out Type? generic, params Type[] arguments) {
+            try {
+                generic = type.MakeGenericType(arguments);
+                return true;
+            } catch {
+                generic = null;
+                return false;
+            }
+        }
+    }
+}
